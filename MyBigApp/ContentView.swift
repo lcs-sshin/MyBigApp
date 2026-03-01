a2989f19b6ae42fd9ed15b439e8d9af9f04bcf1f
Stella Shin <sshin@lcs.on.ca>
2026-03-01 14:19:09 -0500

    Made the text look more normal instead of just floating around the screen.

--- MyBigApp/ContentView.swift
+++ MyBigApp/ContentView.swift
@@ -9,18 +9,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     @State private var selection = "History"
     
     var body: some View {
         NavigationStack{
+            
+            VStack{
+                
+            }
+            
+            VStack(alignment: .leading, spacing:3){
+                Text ("ALSO KNOWN AS")
+                    .font(.caption2)
+                    .foregroundColor(.black)
+                Text("Hermy")
+                    .font(.caption)
+                    .foregroundColor(.black)
+                Text("Know-it-all")
+                    .font(.caption)
+                    .foregroundColor(.black)
+                Text("Miss Grant")
+                    .font(.caption)
+                    .foregroundColor(.black)
+                Text ("Herm-own-ninny")
+                    .font(.caption)
+                    .foregroundColor(.black)
+                
         VStack {
             Spacer()
-            
             Picker("Current Selection",  selection: $selection ) {
                 Text ("Characters") .tag("Characters")
                 Text ("Favourites") .tag("Favourites")
             }
             .pickerStyle(.segmented)
+            
+           
+            }
             }
             
         }
