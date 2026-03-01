25d7ff2414636fa1571e94bf7e092656eb5e6532
Stella Shin <sshin@lcs.on.ca>
2026-03-01 13:09:49 -0500

    Initial Commit

---
+++ MyBigApp/MyBigAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBigAppApp.swift
+//  MyBigApp
+//
+//  Created by zimmer_h on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct MyBigAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
